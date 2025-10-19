6a298c9805a2aa11355300eb7734a53b4c00150c
stefan binoj <stefan.binoj.007@gmail.com>
2025-10-19 05:50:56 +0530

    updated config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,10 +10,11 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 1,
+  retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? "100%" : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["html", { open: process.env.CI ? "never" : "on-failure" }]],
+  reporter: process.env.CI ? [["list"], ["html"]] : "list",
+
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
