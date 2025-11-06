fea3ad594494d275a5154cabf4798ffc3d9a1d4f
stefan binoj <stefan.binoj.007@gmail.com>
2025-11-06 08:33:11 +0530

    added github formatter

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   retries: process.env.CI ? 1 : 0,
   workers: process.env.CI ? "100%" : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? [["list"], ["html"]] : "list",
+  reporter: [ ["list"], ["html", { open: process.env.CI ? "never" : "on-failure" }], ["github"], ],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
